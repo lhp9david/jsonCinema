297bee1a59800ef37c7a890fc7675fbb40a17d58
David Godard <davidgodard76700@gmail.com>
2022-11-28 16:41:52 +0100

    Maj box shadow

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 fetch('movies.json')
-    .then( res => {
+    .then(res => {
         if (res.ok) {
             return res.json();
         }
     })
     .then(data => {
-                function calcStars(rating) {
+        function calcStars(rating) {
 
             let rate = rating / 2
             if (rate <= 1) {
@@ -21,20 +21,20 @@ fetch('movies.json')
                 return (`<img src="img/star.png" alt=""><img src="img/star.png" alt=""><img src="img/star.png" alt=""><img src="img/star.png" alt=""><img src="img/star.png" alt="">`)
             }
         }
-     data.results.forEach(element => {
-        
-       const newDiv = document.createElement('div');
-       newDiv.innerHTML = (`<div class="one">
+        data.results.forEach(element => {
+
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = (`<div class="one">
        <div class="image"><img src="${element.poster_path}" alt=""></div>
        <div class="contenu">
            <div class="title">${element.original_title}</div>
            <div class="description">${element.overview}</div>
            <div class="star">${calcStars(element.vote_average)}</div>
        </div>`);
-     
-       document.querySelector('.grid').appendChild(newDiv);
-  
-     });
+
+            document.querySelector('.grid').appendChild(newDiv);
+
+        });
     });
 
 
